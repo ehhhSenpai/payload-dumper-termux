eb215f9c731ec9c450f7e6cfe5fdfc17d0a60a4b
foxcli <gk13011995@gmail.com>
2025-01-02 03:15:41 +0530

    simplify ui

--- termux-setup.sh
+++ termux-setup.sh
@@ -16,7 +16,7 @@ for i in {10..1}; do
 done
 exit
 
-
+# alternate setup method
 # copy the below line (without #) and run this in termux to setup payload-dumper-termux
 
-# termux-setup-storage && apt update && apt upgrade -y && apt install git python -y && git clone https://github.com/foxcli/payload-dumper-termux.git $HOME/payload-dumper-termux && python -m pip install -r $HOME/payload-dumper-termux/requirements.txt && echo 'export pdtrun="$HOME/payload-dumper-termux/termux-run.sh"' >> $HOME/.bashrc && echo "alias pdt='bash \$pdtrun'" >> $HOME/.bashrc && source $HOME/.bashrc
+# termux-setup-storage && apt update && apt upgrade -y && apt install git python -y && git clone https://github.com/foxcli/payload-dumper-termux.git $HOME/payload-dumper-termux && python -m pip install -r $HOME/payload-dumper-termux/requirements.txt && echo 'export pdtrun="$HOME/payload-dumper-termux/termux-run.sh"' >> $HOME/.bashrc && echo "alias pdt='bash \$pdtrun'" >> $HOME/.bashrc && source $HOME/.bashrc && clear && echo "\n	[ run <pdt> to invoke the dumper ]" && sleep 10
