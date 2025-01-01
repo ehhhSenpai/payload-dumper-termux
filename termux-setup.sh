85f2955ed1f27a997068557e776752c8334e7dae
foxcli <gk13011995@gmail.com>
2025-01-02 03:33:54 +0530

    resolved repo already exist conflict

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,6 +1,7 @@
 termux-setup-storage
 apt update && apt upgrade -y
 apt install git python -y
+rm -rf $HOME/payload-dumper-termux
 git clone https://github.com/foxcli/payload-dumper-termux.git $HOME/payload-dumper-termux
 python -m pip install -r $HOME/payload-dumper-termux/requirements.txt
 echo 'export pdtrun="$HOME/payload-dumper-termux/termux-run.sh"' >> $HOME/.bashrc
@@ -19,4 +20,4 @@ exit
 # alternate setup method
 # copy the below line (without #) and run this in termux to setup payload-dumper-termux
 
-# termux-setup-storage && apt update && apt upgrade -y && apt install git python -y && git clone https://github.com/foxcli/payload-dumper-termux.git $HOME/payload-dumper-termux && python -m pip install -r $HOME/payload-dumper-termux/requirements.txt && echo 'export pdtrun="$HOME/payload-dumper-termux/termux-run.sh"' >> $HOME/.bashrc && echo "alias pdt='bash \$pdtrun'" >> $HOME/.bashrc && source $HOME/.bashrc && clear && echo "\n	[ run <pdt> to invoke the dumper ]" && sleep 10
+# termux-setup-storage && apt update && apt upgrade -y && apt install git python -y && rm -rf $HOME/payload-dumper-termux && git clone https://github.com/foxcli/payload-dumper-termux.git $HOME/payload-dumper-termux && python -m pip install -r $HOME/payload-dumper-termux/requirements.txt && echo 'export pdtrun="$HOME/payload-dumper-termux/termux-run.sh"' >> $HOME/.bashrc && echo "alias pdt='bash \$pdtrun'" >> $HOME/.bashrc && source $HOME/.bashrc && clear && echo "\n [ run <pdt> to invoke the dumper ]" && sleep 10
